54aedae2b9451ed1b101d58e8b46469c6e1bdd2f
Tzu-Yao Huang <tzuyao.jason.huang@gmail.com>
2024-09-04 11:09:43 +0200

    Skeleton for unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
 using InterfaceAdvection
+using WaterLily
 using Test
+
+@testset "PLIC.jl" begin
+    # TODO: true run test
+    @test getIntercept(1,2,3,0.1) == 2
+end
 
 @testset "InterfaceAdvection.jl" begin
     # Write your tests here.
