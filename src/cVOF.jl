f8ed78eb9732ceade58ba052c27ffb0cb2508927
Tzu-Yao Huang (Asus) <tzuyao.jason.huang@gmail.com>
2026-08-18 19:12:20 +0200

    Cosmetic

--- src/cVOF.jl
+++ src/cVOF.jl
@@ -6,8 +6,8 @@ Composite type for 2D or 3D two-phase conservative Volume-of-Fluid (cVOF) advect
 
 The dark fluid is advected using operator-split cVOF method proposed by  [Weymouth & Yue (2010)](https://doi.org/10.1016/j.jcp.2009.12.018).
 This guarentees mass conservation and preserves sharp interface across fluids.
-The primary variable is the volume fraction of the heavy fluid, the cell-averaged color function, `f`. 
-We use Piecewise Linear Interface Calculation (PLIC) to reconstruct sharp interface. 
+The primary variable is the volume fraction of the heavy fluid, the cell-averaged color function, `f`.
+We use Piecewise Linear Interface Calculation (PLIC) to reconstruct sharp interface.
 The dark fluid is indicated with negative distance. That is to say, the normal is pointed into the light fluid.
 """
 struct cVOF{D, T, Sf<:AbstractArray{T}, Vf<:AbstractArray{T}}
@@ -36,9 +36,9 @@ struct cVOF{D, T, Sf<:AbstractArray{T}, Vf<:AbstractArray{T}}
     # domain configuration
     perdir :: NTuple  # tuple of periodic direction
 
-    function cVOF(N::NTuple{D}; 
-                mem=Array, T=Float32, 
-                InterfaceSDF=nothing, 
+    function cVOF(N::NTuple{D};
+                mem=Array, T=Float32,
+                InterfaceSDF=nothing,
                 μ=1e-3, λμ=1e-2, λρ=1e-3, η=nothing,
                 perdir=()
     ) where D
